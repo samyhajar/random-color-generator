8b8fb626f8466d07b90130e10621fb552b9c734f
Samy Hajar <samy.hajar@gmail.com>
2021-01-19 18:35:11 +0100

    Add final changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello baby');
+const randomColor = require('randomcolor'); // import the script
+const chalk = require('chalk'); // import chalk
+
+const color = randomColor({
+  luminosity: process.argv[2],
+  hue: process.argv[3],
+}); // a hex code for an attractive color
+
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('#####                     #####'));
+console.log(chalk.hex(color)('#####       ' + color + '       #####'));
+
+console.log(chalk.hex(color)('#####                     #####'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
