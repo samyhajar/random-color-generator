8be1e8695d33b6eaaa03e529cc40904dc0f5f4e1
Samy Hajar <samy.hajar@gmail.com>
2021-01-22 11:59:36 +0100

    Chnage some mechanics

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const randomColor = require('randomcolor'); // import the script
 const chalk = require('chalk'); // import chalk
 
 const color = randomColor({
-  luminosity: process.argv[2],
-  hue: process.argv[3],
-}); // a hex code for an attractive color
+  hue: process.argv[2],
+  luminosity: process.argv[3],
+}); // a hex code for an attractive col
 
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('#####                     #####'));
-console.log(chalk.hex(color)('#####       ' + color + '       #####'));
-
+console.log(chalk.hex(color)(`#####       ${color}       #####`));
 console.log(chalk.hex(color)('#####                     #####'));
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
